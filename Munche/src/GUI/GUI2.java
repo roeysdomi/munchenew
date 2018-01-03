bb0b53b06b489dfb37ee0fe956b82da0b1ba28c1
adieli313 <34741553+adieli313@users.noreply.github.com>
2018-01-03 03:11:43 +0200

    add some function to the gui almost finshed ver

--- Munche/src/GUI/GUI2.java
+++ Munche/src/GUI/GUI2.java
@@ -34,6 +34,8 @@ import javax.swing.JSeparator;
 import javax.swing.JTextField;
 
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+import com.sun.java.swing.plaf.windows.resources.windows;
+
 import java.awt.TextField;
 import java.awt.event.InputMethodListener;
 import java.awt.event.InputMethodEvent;
@@ -42,6 +44,7 @@ import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
 import javax.swing.JRadioButton;
 import javax.swing.event.ChangeListener;
+import javax.swing.text.DefaultCaret;
 import javax.swing.event.ChangeEvent;
 import java.awt.event.ContainerAdapter;
 import java.awt.event.ContainerEvent;
@@ -66,10 +69,14 @@ public class GUI2 {
 	String foldername;
 	boolean loadfiles=false;
 	int startwith =0; 
+	public static int loadwigile=0;
+	public static int load46col=0;
 	public ArrayList<Wifi> templist=new ArrayList<>();
-
+    public String line4algo2;
     public int counter=0;
+    public int detectfiles=0;
 	public static JFrame frame;
+	public static String path=System.getProperty("user.dir")+"\\";
 	///----------status------------
     final Label MACNUM = new Label("0");
 	final Label WIFINUM = new Label("0");
@@ -90,6 +97,8 @@ public class GUI2 {
 	final JButton load_csv46 = new JButton("Add+");
 	final JButton lunch_algo1 = new JButton("Lunch!");
 	final JButton lunch_aglo2_line = new JButton("Lunch!");
+	final JButton algo2_lunch_macs = new JButton("Lunch!");
+
 
 	///--------text input------------------
 	final TextField textid = new TextField();
@@ -103,7 +112,12 @@ public class GUI2 {
 	final TextField file_csv46 = new TextField();
 	final TextField mac_algo1 = new TextField();
 	final TextField algo2_line = new TextField();
-
+	final TextField algo2_sig_1 = new TextField();
+	final TextField algo2_sig_2 = new TextField();
+	final TextField algo2_sig_3 = new TextField();
+	final TextField algo2_mac_1 = new TextField();
+	final TextField algo2_mac_2 = new TextField();
+	final TextField algo2_mac_3 = new TextField();
 	public final static TextField wifilimit = new TextField();
 
 
@@ -114,46 +128,56 @@ public class GUI2 {
 	final JRadioButton checkbox2 = new JRadioButton("");
 	final JRadioButton checkbox3 = new JRadioButton("");
 	
-	
+	//--------------------------------------------
 	
 	/**
 	 * Launch the application.
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		GUI2 ron = new GUI2();
+		ron.GUI21(ron);
+		
+		ron.frame.setVisible(true);
+		
+		/*
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					GUI2 window = new GUI2();
 					window.frame.setVisible(true);
-					
+					checkfiles(obj);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
+		*/
 	}
 
 	/**
-	 * Create the application.
+	
+ * Create the application.
 	 * @throws IOException 
 	 */
-	public GUI2() throws IOException {
+	public GUI2() {};
+	public void GUI21(GUI2 obj) throws IOException {
 		Design();
 		Design2();
-		loadwigle();
+		loadwigle(obj);
 		lunchbutton();
 		filtertype();
 		goback();
 		reset();
-		load46col();
+		load46col(obj);
 		savefilter();
 		loadsavedfilter();
 		saved46col();
 		savetokml();
 		lunchalgo1();
 		lunchalgo2line();
-		
-		
+		lunchalgo2macs();
+		checkfiles( obj);
 		
 	}
     
@@ -180,13 +204,13 @@ public class GUI2 {
 		JSeparator separator3 = new JSeparator();
 		separator3.setOrientation(SwingConstants.VERTICAL);
 		separator3.setForeground(new Color(192, 192, 192));
-		separator3.setBounds(357, 199, 8, 161);
+		separator3.setBounds(357, 193, 8, 167);
 		frame.getContentPane().add(separator3);
 		
 		JSeparator separator4 = new JSeparator();
 		separator4.setOrientation(SwingConstants.VERTICAL);
 		separator4.setForeground(new Color(192, 192, 192));
-		separator4.setBounds(463, 199, 8, 161);
+		separator4.setBounds(463, 169, 8, 191);
 		frame.getContentPane().add(separator4);
 		
 		JLabel lblType = DefaultComponentFactory.getInstance().createLabel("Type:");
@@ -220,6 +244,10 @@ public class GUI2 {
 		JSeparator separator_9 = new JSeparator();
 		separator_9.setBounds(0, 412, 624, 4);
 		frame.getContentPane().add(separator_9);
+		
+		JSeparator separator_10 = new JSeparator();
+		separator_10.setBounds(0, 441, 624, 4);
+		frame.getContentPane().add(separator_10);
 		
 		JLabel lblLoadFiles = DefaultComponentFactory.getInstance().createLabel("LOAD FILES :");
 		lblLoadFiles.setFont(new Font("Tahoma", Font.BOLD, 11));
@@ -272,7 +300,7 @@ public class GUI2 {
 		
 		frame = new JFrame();
 		frame.getContentPane().setBackground(SystemColor.control);
-		frame.setBounds(100, 100, 650, 591);
+		frame.setBounds(100, 100, 650, 664);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		textArea.setBounds(0, 2, 128, 4);
@@ -364,10 +392,79 @@ public class GUI2 {
 		counthis.setBounds(105, 10, 23, 17);
 		frame.getContentPane().add(counthis);
 			/////-------------פתור לאנץ'---------------
-	
-	    ///--------type button--------
-
+		JLabel lblAlgo = DefaultComponentFactory.getInstance().createLabel("<HTML>ALGO 1: MAC:</HTML>");
+		lblAlgo.setBounds(10, 377, 78, 33);
+		frame.getContentPane().add(lblAlgo);
+		JLabel lblAlgo2 = DefaultComponentFactory.getInstance().createLabel("<HTML>ALGO 2: LINE:</HTML>");
+		lblAlgo2.setBounds(10, 412, 78, 33);
+		frame.getContentPane().add(lblAlgo2);
 		
+		mac_algo1.setBounds(94, 384, 239, 20);
+		frame.getContentPane().add(mac_algo1);
+		mac_algo1.setColumns(10);
+		
+		algo2_line.setBounds(94, 418, 325, 20);
+		frame.getContentPane().add(algo2_line);
+		algo2_line.setColumns(10);
+		
+		
+		lunch_algo1.setBounds(339, 371, 89, 40);
+		frame.getContentPane().add(lunch_algo1);
+		
+		JLabel lblWifiLimit = DefaultComponentFactory.getInstance().createLabel("<html>Wifi limit:</html>");
+		lblWifiLimit.setBounds(442, 377, 71, 33);
+		frame.getContentPane().add(lblWifiLimit);
+		
+		wifilimit.setBounds(532, 384, 92, 22);
+		frame.getContentPane().add(wifilimit);
+		wifilimit.setColumns(10);
+		
+		lunch_aglo2_line.setBounds(425, 417, 89, 23);
+		frame.getContentPane().add(lunch_aglo2_line);
+		
+		JLabel lblAlgoBy = DefaultComponentFactory.getInstance().createLabel("ALGO2 : BY MACS:");
+		lblAlgoBy.setBounds(10, 454, 118, 14);
+		frame.getContentPane().add(lblAlgoBy);
+		
+		JLabel lblMac = DefaultComponentFactory.getInstance().createLabel("mac 1:");
+		lblMac.setBounds(57, 490, 49, 14);
+		frame.getContentPane().add(lblMac);
+		
+		JLabel lblMac_1 = DefaultComponentFactory.getInstance().createLabel("mac 2:");
+		lblMac_1.setBounds(56, 533, 49, 14);
+		frame.getContentPane().add(lblMac_1);
+		
+		JLabel lblMac_2 = DefaultComponentFactory.getInstance().createLabel("mac 3:");
+		lblMac_2.setBounds(57, 571, 43, 14);
+		frame.getContentPane().add(lblMac_2);
+		
+		JLabel lblSignalMac = DefaultComponentFactory.getInstance().createLabel("Signal    ,  mac");
+		lblSignalMac.setBounds(140, 471, 92, 14);
+		frame.getContentPane().add(lblSignalMac);
+		
+		algo2_sig_1.setBounds(133, 490, 39, 19);
+		frame.getContentPane().add(algo2_sig_1);
+		
+		algo2_sig_2.setBounds(133, 533, 40, 19);
+		frame.getContentPane().add(algo2_sig_2);
+		
+		algo2_sig_3.setBounds(133, 571, 39, 19);
+		frame.getContentPane().add(algo2_sig_3);
+		
+		algo2_mac_1.setBounds(175, 490, 158, 19);
+		frame.getContentPane().add(algo2_mac_1);
+		
+		
+		algo2_mac_2.setBounds(175, 533, 158, 19);
+		frame.getContentPane().add(algo2_mac_2);
+		
+		algo2_mac_3.setBounds(175, 571, 158, 19);
+		frame.getContentPane().add(algo2_mac_3);
+		
+		
+		algo2_lunch_macs.setBounds(398, 501, 128, 51);
+		frame.getContentPane().add(algo2_lunch_macs);
+
 		
 		
 		
@@ -474,12 +571,14 @@ public class GUI2 {
 		lunch.setBounds(151, 100, 89, 51);
 		frame.getContentPane().add(lunch);
 	}
-	public void loadwigle()
+	public void loadwigle(final GUI2 obj)
 	{
 		btnLoad.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				if(loadwigile==0&&load46col==0) {checkfiles(obj);}
 				loadfiles=true;
 				startwith=1;
+				loadwigile=1;
 				Read4GUI re=new Read4GUI();
 				
 				String foldername=loadfiles_folder.getText();
@@ -597,7 +696,7 @@ public class GUI2 {
 		
 		JLabel lblSaveFile = new JLabel("SAVE FILE:");
 		lblSaveFile.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblSaveFile.setBounds(463, 174, 71, 14);
+		lblSaveFile.setBounds(469, 174, 78, 14);
 		frame.getContentPane().add(lblSaveFile);
 		
 		
@@ -642,6 +741,8 @@ public class GUI2 {
 		    	MACNUM.setText("0");
 		    	counthis.setText("0");
 		    	counter=0;
+		    	loadwigile=0;
+		    	load46col=0;
 			}
 		});
 
@@ -664,7 +765,7 @@ public class GUI2 {
     	checkbox3.setSelected(false);
     	
     	/////-------------------
-    	if(startwith==1) {
+    	if(loadwigile==1) {
     	loadfiles=true;
 		Read4GUI re=new Read4GUI();
 		
@@ -687,7 +788,7 @@ public class GUI2 {
 			e1.printStackTrace();
 		}
     	}
-    	if(startwith==2)
+    	if(load46col==1)
     	{
     		Read4GUI re=new Read4GUI();
 			startwith=2;
@@ -704,7 +805,9 @@ public class GUI2 {
 			}
 			status();
     	}
+    	textArea.setText("");
     	printlist();
+    	textArea.append("\n  FILES WERE REALOADED !\n");
 
     }
     public void printlist()
@@ -718,15 +821,20 @@ public class GUI2 {
 					","+templist.get(i).getLot()+
 					 "\n");
 		}
+    	  DefaultCaret caret = (DefaultCaret)textArea.getCaret();
+    	   caret.setUpdatePolicy(DefaultCaret.OUT_BOTTOM);    	
+    	   recountlines(templist);
     }
-    public void load46col()
+    public void load46col(final GUI2 obj)
     {
     	folder_csv46.setText("INPUT\\target\\");
 		file_csv46.setText("_comb_all_BM2_.csv");
 		load_csv46.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if(loadwigile==0&&load46col==0) {checkfiles(obj);}
 				Read4GUI re=new Read4GUI();
 				startwith=2;
+				load46col=1;
 				String foldername=folder_csv46.getText();
 				String filename=file_csv46.getText();
 				try {
@@ -746,36 +854,7 @@ public class GUI2 {
 		load_csv46.setBounds(258, 320, 89, 33);
 		frame.getContentPane().add(load_csv46);
 		
-		JLabel lblAlgo = DefaultComponentFactory.getInstance().createLabel("<HTML>ALGO 1: MAC:</HTML>");
-		lblAlgo.setBounds(10, 377, 78, 33);
-		frame.getContentPane().add(lblAlgo);
-		JLabel lblAlgo2 = DefaultComponentFactory.getInstance().createLabel("<HTML>ALGO 2: LINE:</HTML>");
-		lblAlgo2.setBounds(10, 412, 78, 33);
-		frame.getContentPane().add(lblAlgo2);
 		
-		mac_algo1.setBounds(94, 384, 239, 20);
-		frame.getContentPane().add(mac_algo1);
-		mac_algo1.setColumns(10);
-		
-		algo2_line.setBounds(94, 418, 325, 20);
-		frame.getContentPane().add(algo2_line);
-		algo2_line.setColumns(10);
-		
-		
-		lunch_algo1.setBounds(339, 371, 89, 40);
-		frame.getContentPane().add(lunch_algo1);
-		
-		JLabel lblWifiLimit = DefaultComponentFactory.getInstance().createLabel("<html>Wifi limit:</html>");
-		lblWifiLimit.setBounds(442, 377, 71, 33);
-		frame.getContentPane().add(lblWifiLimit);
-		
-		wifilimit.setBounds(532, 384, 92, 22);
-		frame.getContentPane().add(wifilimit);
-		wifilimit.setColumns(10);
-		
-		lunch_aglo2_line.setBounds(425, 417, 89, 23);
-		frame.getContentPane().add(lunch_aglo2_line);
-
     	
     }
     public void savefilter() throws IOException
@@ -872,13 +951,13 @@ public class GUI2 {
     public void lunchalgo1()
     {   mac_algo1.setText("90:6c:ac:a0:3d:eb");
     	wifilimit.setText("5");
-    	Calculate1 cal=new Calculate1();
-    	cal.a1=wifilimit.getText();
+    	
     	lunch_algo1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Write4GUI re=new Write4GUI();
 				String mac= mac_algo1.getText();
-				
+				Calculate1 cal=new Calculate1();
+		    	cal.a1=wifilimit.getText();
 				
 				try {
 					re.WriteMacalgo(templist, mac);
@@ -893,14 +972,17 @@ public class GUI2 {
 		});
     }
     public void lunchalgo2line()
-    {     algo2_line.setText("12/05/17 11:41 AM,model=SM-G950F_device=dreamlte,?,?,?,1,Guest,90:6c:ac:a0:3d:eb,3,-76");
+    {     algo2_line.setText("12/05/17 11:41 AM,model=SM-G950F_device=dreamlte,?,?,?,3,Guest,1c:b9:c4:15:ed:b8,3,-81,Guest,8c:0c:90:ae:16:83,3,-86,Guest,1c:b9:c4:16:ed:3c,3,-91");
+          
     	lunch_aglo2_line.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				String line=algo2_line.getText();
+				line4algo2=algo2_line.getText();
 				Read4GUI read=new Read4GUI();
+				Calculate1 cal=new Calculate1();
+		    	cal.a1=wifilimit.getText();
 				Write4GUI writer=new Write4GUI();
 				try {
-					writer.targetlist=read.ReadeNoGPSCsv(line);
+					writer.targetlist=read.ReadeNoGPSCsv(line4algo2);
 					writer.traininglist=new ArrayList<>(templist);
 					writer.WriteLiner();
 					textArea.append("\n RESULT(ALGO2 BY LINE): \n");
@@ -913,7 +995,66 @@ public class GUI2 {
 		});
 		
     }
+    public void lunchalgo2macs()
+    {    
+    	
+    	algo2_sig_1.setText("-81");
+    	algo2_mac_1.setText("1c:b9:c4:15:ed:b8");
+    	algo2_sig_2.setText("-86");
+    	algo2_mac_2.setText("8c:0c:90:ae:16:83");
+    	algo2_sig_3.setText("-91");
+    	algo2_mac_3.setText("1c:b9:c4:16:ed:3c");
 
+    	
+
+    	
+    	algo2_lunch_macs.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			if(algo2_sig_1.getText()!=null) {line4algo2="12/05/17 11:41 AM,model=SM-G950F_device=dreamlte,?,?,?,1,Guest,"+algo2_mac_1.getText()+",3,"+algo2_sig_1.getText()+",";}
+			if(algo2_sig_2.getText()!=null)  {line4algo2="12/05/17 11:41 AM,model=SM-G950F_device=dreamlte,?,?,?,2,Guest,"+algo2_mac_1.getText()+",3,"+algo2_sig_1.getText()+","+"Guest,"+algo2_mac_2.getText()+",3,"+algo2_sig_2.getText()+",";;}
+			if(algo2_sig_3.getText()!=null)  {line4algo2="12/05/17 11:41 AM,model=SM-G950F_device=dreamlte,?,?,?,3,Guest,"+algo2_mac_1.getText()+",3,"+algo2_sig_1.getText()+","+"Guest,"+algo2_mac_2.getText()+",3,"+algo2_sig_2.getText()+","+"Guest,"+algo2_mac_3.getText()+",3,"+algo2_sig_3.getText()+",";;;}
+			Calculate1 cal=new Calculate1();
+	    	cal.a1=wifilimit.getText();
+			Read4GUI read=new Read4GUI();
+			Write4GUI writer=new Write4GUI();
+			try {
+				writer.targetlist=read.ReadeNoGPSCsv(line4algo2);
+				writer.traininglist=new ArrayList<>(templist);
+				writer.WriteLiner();
+				textArea.append("\n RESULT(ALGO2 BY MACS:): \n");
+				textArea.append("\n" +writer.resultalgo2_line+"\n");
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+			
+			}
+		});
+    }
+    public void recountlines(ArrayList<Wifi> list)
+    {   int counter=1;
+        int remember=1;
+        remember=templist.get(0).getLine();
+    	for(int i=0;i<templist.size();i++)
+    	{
+    		
+    		if(templist.get(i).getLine()!=remember)
+    		{remember=templist.get(i).getLine();counter++;templist.get(i).setLine(counter);}
+    		if(templist.get(i).getLine()==remember)
+    		{templist.get(i).setLine(counter);   }
+    		
+    	}
+    }
+    public static void checkfiles(GUI2 obj)
+    {
+    	
+    	checkfiles4GUI detectfiles=new checkfiles4GUI(path+"csv\\", path+"INPUT\\target\\",obj);
+		Thread dani=new Thread(detectfiles);
+		dani.start();
+		
+		
+    }
 }
 
 
